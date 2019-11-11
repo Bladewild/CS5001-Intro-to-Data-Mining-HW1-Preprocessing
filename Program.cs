fcf7abf0dff16b2a780680e6822c2a70a6c98b55
Bladewild <blade_wild@yahoo.com>
2019-11-11 01:48:53 -0600

    Updated stuff. Missing Values in nominal data (Anything that is not a float) will be filled in with the majority nominal value in that coloumn

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 namespace HW1
 {
     class Program
@@ -22,23 +21,173 @@ namespace HW1
                 return "[" + error + "," + fix + "]";
             }
         }
-        
+
+        struct ValueOccurence
+        {
+            public string value;
+            public int numoccurence;
+            public ValueOccurence(string inputValue, int inputOccurence)
+            {
+                value = inputValue;
+                numoccurence = inputOccurence;
+            }
+
+            public ValueOccurence(string inputValue)
+            {
+                value = inputValue;
+                numoccurence = 1;
+            }
+            /*
+            public void Increase()
+            {
+                numoccurence++;
+            }*/
+
+            public override string ToString()
+            {
+                return "[" + value + "-timesoccured" + numoccurence + "]";
+            }
+        }
+
+        struct Coloumn
+        {
+            private static string nominal = "nominal";
+            private static string numeric = "numeric";
+            public string title;
+            public List<string> instances;
+            public List<ValueOccurence> ValueEntries;//for nominal 
+            public ValueOccurence majorityValue;//for nominal 
+            public List<int> MissingValueIndexes;
+
+            public string type;
+
+            public Coloumn(string titleinput)
+            {
+                MissingValueIndexes = new List<int>();
+                majorityValue = new ValueOccurence("NONE", -1);
+                ValueEntries = new List<ValueOccurence>();
+                instances = new List<string>();
+                title = titleinput;
+                string result;
+
+                switch (title)
+                {
+                    case "\"is_attack\"":
+                    case "\"P101\"":
+                    case "\"P203\"":
+                    case "\"P102\"":
+                    case "\"P205\"":
+                    case "\"P206\"":
+                    case "\"P301\"":
+                    case "\"P302\"":
+                    case "\"P401\"":
+                    case "\"P402\"":
+                    case "\"P403\"":
+                    case "\"P404\"":
+                    case "\"P501\"":
+                    case "\"P502\"":
+                    case "\"P601\"":
+                    case "\"P602\"":
+                    case "\"P603\"":
+                    case "\"P201\"":
+                    case "\"P202\"":
+                    case "\"P204\"":
+                    case "\"MV301\"":
+                    case "\"MV302\"":
+                    case "\"MV304\"":
+                    case "\"UV401\"":
+                    case "\"MV101\"":
+                    case "\"MV201\"":
+                    case "\"MV303\"":
+                        result = nominal;
+                        break;
+                    default:
+                        result = numeric;
+                        break;
+                }
+                type = result;
+            }
+
+            public void AddValue(string value)
+            {
+                if (type == nominal)
+                {
+                    bool found = false;
+                    for (int i = 0; i < ValueEntries.Count; i++)
+                    {
+                        string test = ValueEntries[i].value;
+                        if (ValueEntries[i].value == value)
+                        {
+                            ValueOccurence newVO = new ValueOccurence(ValueEntries[i].value, ValueEntries[i].numoccurence + 1);
+                            ValueEntries[i] = newVO;
+                            //Console.WriteLine("INCREASED: "+ValueEntries[i].value+":"+ValueEntries[i].numoccurence);
+                            found = true;
+                            break;
+                        }
+
+                    }
+                    if (value != "")
+                    {
+                        if (found == false)
+                            ValueEntries.Add(new ValueOccurence(value));
+
+                    }
+                    else
+                    {
+                        MissingValueIndexes.Add(instances.Count);
+                    }
+                }
+                instances.Add(value);
+
+            }
+            public void UpdateMajorityValue()
+            {
+                
+                foreach (var item in ValueEntries)
+                {
+                    if (item.numoccurence > this.majorityValue.numoccurence)
+                    {
+                        //Console.WriteLine("LAST MV: " + this.majorityValue.value);
+                        this.majorityValue = item;
+                        //Console.WriteLine("CURRENT MV: " + this.majorityValue.value);
+                    }
+                }
+            }
+            public override string ToString()
+            {
+                string output = "--" + title + "--\n";
+                foreach (var row in instances)
+                {
+                    output += "row\n";
+                }
+                output += "--" + title + "--\n";
+                return output;
+            }
+
+        }
+
         static List<FixValue> FixList = new List<FixValue>();
-        
+
         static void Main(string[] args)
         {
             //Can be made into seperate function where input is read through text
-            FixList.Add( new FixValue("N","0"));
-            FixList.Add( new FixValue("Y","1"));
-            FixList.Add( new FixValue("NA",""));
-            FixList.Add( new FixValue("\"N\"","0"));
-            FixList.Add( new FixValue("\"Y\"","1"));
+            FixList.Add(new FixValue("N", "0"));
+            FixList.Add(new FixValue("Y", "1"));
+            FixList.Add(new FixValue("NA", ""));
+            FixList.Add(new FixValue("\"N\"", "0"));
+            FixList.Add(new FixValue("\"Y\"", "1"));
 
+            List<Coloumn> table = new List<Coloumn>();
             List<List<string>> Lines = new List<List<string>>();
             string fread = "5402_dataset.csv";
             string fwrite = "5402_datasetfix.csv";
             Lines = ReadFile(fread);
             Lines = ProprocessLines(Lines);
+
+            table = CreateTable(Lines);
+            table = FixNominals(table);
+
+            Lines = ConvertBackIntoLines(table);
             WriteFile(fwrite, Lines);
 
 
@@ -76,7 +225,7 @@ namespace HW1
                 List<string> inputLine = new List<string>();
                 inputLine.AddRange(input);
                 linecontainer.Add(inputLine);
-                Console.WriteLine(OutputLine(inputLine));
+                //Console.WriteLine(OutputLine(inputLine));
                 //Console.WriteLine("-------------------------------------------");
             }
             /*
@@ -86,6 +235,95 @@ namespace HW1
             // to close the stream 
             sr.Close();
             return linecontainer;
+        }
+
+        private static List<Coloumn> CreateTable(List<List<string>> linestoProcess)
+        {
+
+            List<Coloumn> table = new List<Coloumn>();
+
+            //create columns
+            foreach (string title in linestoProcess[0])
+            {
+                table.Add(new Coloumn(title));
+            }
+            for (int i = 1; i < linestoProcess.Count; i++)
+            {
+                int index = 0;
+                foreach (var item in linestoProcess[i])
+                {
+                    table[index].AddValue(item);
+                    index++;
+                }
+            }
+            return table;
+
+        }
+
+        private static List<Coloumn> FixNominals(List<Coloumn> tabletoProcess)
+        {
+
+            for (int i = 0; i < tabletoProcess.Count; i++)
+            {
+                if (tabletoProcess[i].type == "nominal")
+                {
+                    //figure out majority number// already done with nominal automitically
+                    //process
+
+                    Coloumn coloumn = tabletoProcess[i];
+                    /*
+                    Console.WriteLine(coloumn.title+": "+ coloumn.MissingValueIndexes.Count);
+                    
+                    foreach (var item in coloumn.ValueEntries)
+                    {
+                        Console.WriteLine(tabletoProcess[i].type);
+                        Console.WriteLine(item);
+                        
+                    }*/
+                   // Console.WriteLine("BEFORE");
+                    //Console.WriteLine(coloumn.MissingValueIndexes.Count + ":" + coloumn.majorityValue.value);
+
+                    coloumn.UpdateMajorityValue();
+                    //Console.WriteLine("AFTER");
+                    //Console.WriteLine(coloumn.MissingValueIndexes.Count + ":" + coloumn.majorityValue.value);
+
+                    for (int j = 0; j < coloumn.MissingValueIndexes.Count; j++)
+                    {
+
+                        coloumn.instances[coloumn.MissingValueIndexes[j]] = coloumn.majorityValue.value;
+                    }
+                    tabletoProcess[i] = coloumn;
+                }
+            }
+            return tabletoProcess;
+
+        }
+
+        private static List<List<string>> ConvertBackIntoLines(List<Coloumn> coloumnstoConvert)
+        {
+
+            List<List<string>> linestoReturn = new List<List<string>>();
+            List<string> linetoAdd = new List<string>();
+
+            //create columns
+            foreach (Coloumn column in coloumnstoConvert)
+            {
+                linetoAdd.Add(column.title);
+            }
+            linestoReturn.Add(linetoAdd);
+            int numRows = coloumnstoConvert[0].instances.Count;
+            for (int i = 1; i < numRows; i++)
+            {
+                linetoAdd = new List<string>();
+                foreach (Coloumn column in coloumnstoConvert)
+                {
+                    linetoAdd.Add(column.instances[i]);
+                }
+                linestoReturn.Add(linetoAdd);
+            }
+
+            return linestoReturn;
+
         }
 
         private static void WriteFile(string filetoWrite, List<List<string>> LinestoWrite)
@@ -132,11 +370,11 @@ namespace HW1
                 string stringtomodify = linetoModify[i];
                 for (int j = 0; j < FixList.Count; j++)
                 {
-                    if(stringtomodify == FixList[j].error)
+                    if (stringtomodify == FixList[j].error)
                     {
-                        linetoModify[i]=FixList[j].fix;                           
+                        linetoModify[i] = FixList[j].fix;
                         break;
-                    }                 
+                    }
                 }
             }
             return linetoModify;
